80647ac1b383432dea0e1408546dd89c88e0ad1c
ByteHamster <info@bytehamster.com>
2022-10-18 17:17:47 +0200

    More detailed measurements

--- scripts/leafMethods.sh
+++ scripts/leafMethods.sh
@@ -3,11 +3,11 @@ hostname
 strings recsplit_construction | grep " -m"
 
 # shellcheck disable=SC2086
-for leafSize in $(seq 6 2 30); do
-    for bucketSize in 100 500 1000 2000; do
-        params="--numObjects 1k --numQueries 0 --leafSize $leafSize --bucketSize $bucketSize"
+for leafSize in $(seq 2 1 34); do
+    for bucketSize in 50 100 500 1000 1500 2000; do
+        params="--numObjects 5M --numQueries 0 --leafSize $leafSize --bucketSize $bucketSize"
 
-        if [[ "$leafSize" -lt 16 ]]; then
+        if [[ "$leafSize" -lt 18 ]]; then
           ./recsplit_construction $params --leafMethod bruteforce
           ./recsplit_construction $params --leafMethod rotations
         fi
