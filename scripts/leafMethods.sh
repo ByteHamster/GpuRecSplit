444424a052576fa98623993dcd1f231cf03502e3
ByteHamster <info@bytehamster.com>
2022-10-19 09:28:38 +0200

    Speed up leafMethod experiment

--- scripts/leafMethods.sh
+++ scripts/leafMethods.sh
@@ -3,14 +3,17 @@ hostname
 strings recsplit_construction | grep " -m"
 
 # shellcheck disable=SC2086
-for leafSize in $(seq 2 1 34); do
-    for bucketSize in 50 100 500 1000 1500 2000; do
+for leafSize in $(seq 2 1 16); do
+    for bucketSize in 100 500 1000 2000; do
         params="--numObjects 5M --numQueries 0 --leafSize $leafSize --bucketSize $bucketSize"
+        ./recsplit_construction $params --leafMethod bruteforce
+        ./recsplit_construction $params --leafMethod rotations
+    done
+done
 
-        if [[ "$leafSize" -lt 18 ]]; then
-          ./recsplit_construction $params --leafMethod bruteforce
-          ./recsplit_construction $params --leafMethod rotations
-        fi
+for leafSize in $(seq 2 2 34); do
+    for bucketSize in 100 500 1000 2000; do
+        params="--numObjects 5M --numQueries 0 --leafSize $leafSize --bucketSize $bucketSize"
         ./recsplit_construction $params --leafMethod cuckoo
     done
 done
