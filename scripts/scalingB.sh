d99d555af7477df644aa5d4ca5641aad5f083aef
ByteHamster <info@bytehamster.com>
2022-11-10 14:29:09 +0100

    Slightly larger test

--- scripts/scalingB.sh
+++ scripts/scalingB.sh
@@ -4,8 +4,8 @@ strings gpurecsplit_construction | grep " -m"
 strings simdrecsplit_construction | grep " -m"
 
 # shellcheck disable=SC2086
-for bucketSize in $(seq 100 100 2000); do
-  params="--numObjects 20M --numQueries 0 --leafMethod rotations --leafSize 8 --bucketSize $bucketSize"
+for bucketSize in $(seq 50 50 500); do
+  params="--numObjects 50M --numQueries 0 --leafMethod rotations --leafSize 10 --bucketSize $bucketSize"
   ./gpurecsplit_construction $params --numThreads 1
   ./simdrecsplit_construction $params --numThreads 1
   ./gpurecsplit_construction $params --numThreads 8
