a097236f45107b39dff05b8316926fe8c494450a
ByteHamster <info@bytehamster.com>
2022-11-11 14:09:40 +0100

    Smaller scaling benchmark

--- scripts/scaling.sh
+++ scripts/scaling.sh
@@ -5,18 +5,18 @@ strings gpurecsplit_construction | grep " -m"
 strings simdrecsplit_construction | grep " -m"
 
 for threads in $(seq 1 1 8); do
-  params="--numObjects $((20*threads))M --numQueries 0 --leafMethod rotations --leafSize 5 --bucketSize 5 --numThreads $threads"
-  [ $threads == 1 ] && ./recsplit_construction $params
-  ./gpurecsplit_construction $params
-  ./simdrecsplit_construction $params
+  params="--numQueries 0 --leafMethod rotations --leafSize 5 --bucketSize 5 --numThreads $threads"
+  [ $threads == 1 ] && ./recsplit_construction $params "--numObjects $((20*threads))M"
+  ./gpurecsplit_construction $params "--numObjects $((2*threads))M"
+  ./simdrecsplit_construction $params "--numObjects $((20*threads))M"
 
   params="--numObjects $((20*threads))M --numQueries 0 --leafMethod rotations --leafSize 8 --bucketSize 100 --numThreads $threads"
   [ $threads == 1 ] && ./recsplit_construction $params
   ./gpurecsplit_construction $params
   ./simdrecsplit_construction $params
 
-  params="--numObjects $((20*threads))M --numQueries 0 --leafMethod rotations --leafSize 16 --bucketSize 2000 --numThreads $threads"
-  [ $threads == 1 ] && ./recsplit_construction $params
-  ./gpurecsplit_construction $params
-  ./simdrecsplit_construction $params
+  params="--numQueries 0 --leafMethod rotations --leafSize 16 --bucketSize 2000 --numThreads $threads"
+  [ $threads == 1 ] && ./recsplit_construction $params --numObjects "$(threads)M"
+  ./gpurecsplit_construction $params --numObjects "$(threads)M"
+  ./simdrecsplit_construction $params --numObjects "$(threads)M"
 done
