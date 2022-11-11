47ea845f417dc083a3b2664d17015704ef546bee
ByteHamster <info@bytehamster.com>
2022-11-11 14:10:18 +0100

    Fix

--- scripts/scaling.sh
+++ scripts/scaling.sh
@@ -6,9 +6,9 @@ strings simdrecsplit_construction | grep " -m"
 
 for threads in $(seq 1 1 8); do
   params="--numQueries 0 --leafMethod rotations --leafSize 5 --bucketSize 5 --numThreads $threads"
-  [ $threads == 1 ] && ./recsplit_construction $params "--numObjects $((20*threads))M"
-  ./gpurecsplit_construction $params "--numObjects $((2*threads))M"
-  ./simdrecsplit_construction $params "--numObjects $((20*threads))M"
+  [ $threads == 1 ] && ./recsplit_construction $params --numObjects $((20*threads))M
+  ./gpurecsplit_construction $params --numObjects $((2*threads))M
+  ./simdrecsplit_construction $params --numObjects $((20*threads))M
 
   params="--numObjects $((20*threads))M --numQueries 0 --leafMethod rotations --leafSize 8 --bucketSize 100 --numThreads $threads"
   [ $threads == 1 ] && ./recsplit_construction $params
