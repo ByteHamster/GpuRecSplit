25e008d319128ed6c5edc0d5bc78d0c116d12b70
ByteHamster <info@bytehamster.com>
2022-11-11 15:58:22 +0100

    Tuning for comparable benchmark performance

--- scripts/scaling.sh
+++ scripts/scaling.sh
@@ -7,17 +7,17 @@ strings simdrecsplit_construction | grep " -m"
 for threads in $(seq 1 1 16); do
   params="--numQueries 0 --leafMethod rotations --leafSize 5 --bucketSize 5 --numThreads $threads"
   ./gpurecsplit_construction $params --numObjects $((2*threads))M
-  ./simdrecsplit_construction $params --numObjects $((20*threads))M
+  ./simdrecsplit_construction $params --numObjects $((50*threads))M
 
-  params="--numObjects $((20*threads))M --numQueries 0 --leafMethod rotations --leafSize 8 --bucketSize 100 --numThreads $threads"
-  ./gpurecsplit_construction $params
-  ./simdrecsplit_construction $params
+  params="--numQueries 0 --leafMethod rotations --leafSize 8 --bucketSize 100 --numThreads $threads"
+  ./gpurecsplit_construction $params --numObjects $((20*threads))M
+  ./simdrecsplit_construction $params --numObjects $((30*threads))M
 
-  params="--numObjects $((20*threads))M --numQueries 0 --leafMethod rotations --leafSize 12 --bucketSize 9 --numThreads $threads"
-  ./gpurecsplit_construction $params
-  ./simdrecsplit_construction $params
+  params="--numQueries 0 --leafMethod rotations --leafSize 12 --bucketSize 9 --numThreads $threads"
+  ./gpurecsplit_construction $params --numObjects $((5*threads))M
+  ./simdrecsplit_construction $params --numObjects $((10*threads))M
 
-  params="--numObjects $((200*threads))k --numQueries 0 --leafMethod rotations --leafSize 16 --bucketSize 2000 --numThreads $threads"
-  ./gpurecsplit_construction $params
-  ./simdrecsplit_construction $params
+  params="--numQueries 0 --leafMethod rotations --leafSize 16 --bucketSize 2000 --numThreads $threads"
+  ./gpurecsplit_construction $params --numObjects $((500*threads))k
+  ./simdrecsplit_construction $params --numObjects $((20*threads))k
 done
