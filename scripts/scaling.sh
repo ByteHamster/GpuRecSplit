18cd2477cd97d90f293df5f4642d47e83c8ee393
ByteHamster <info@bytehamster.com>
2022-11-11 14:46:35 +0100

    Yet another way to measure speedups

--- scripts/scaling.sh
+++ scripts/scaling.sh
@@ -4,19 +4,20 @@ hostname
 strings gpurecsplit_construction | grep " -m"
 strings simdrecsplit_construction | grep " -m"
 
-for threads in $(seq 1 1 8); do
+for threads in $(seq 1 1 16); do
   params="--numQueries 0 --leafMethod rotations --leafSize 5 --bucketSize 5 --numThreads $threads"
-  [ $threads == 1 ] && ./recsplit_construction $params --numObjects $((20*threads))M
   ./gpurecsplit_construction $params --numObjects $((2*threads))M
   ./simdrecsplit_construction $params --numObjects $((20*threads))M
 
   params="--numObjects $((20*threads))M --numQueries 0 --leafMethod rotations --leafSize 8 --bucketSize 100 --numThreads $threads"
-  [ $threads == 1 ] && ./recsplit_construction $params
   ./gpurecsplit_construction $params
   ./simdrecsplit_construction $params
 
-  params="--numQueries 0 --leafMethod rotations --leafSize 16 --bucketSize 2000 --numThreads $threads"
-  [ $threads == 1 ] && ./recsplit_construction $params --numObjects "$((200*threads))k"
-  ./gpurecsplit_construction $params --numObjects "$((200*threads))k"
-  ./simdrecsplit_construction $params --numObjects "$((200*threads))k"
+  params="--numObjects $((20*threads))M --numQueries 0 --leafMethod rotations --leafSize 12 --bucketSize 9 --numThreads $threads"
+  ./gpurecsplit_construction $params
+  ./simdrecsplit_construction $params
+
+  params="--numObjects $((200*threads))k --numQueries 0 --leafMethod rotations --leafSize 16 --bucketSize 2000 --numThreads $threads"
+  ./gpurecsplit_construction $params
+  ./simdrecsplit_construction $params
 done
