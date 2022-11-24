2727c7851a9b8d89531b3224ab38e885b186e91e
ByteHamster <info@bytehamster.com>
2022-11-24 11:59:48 +0100

    Add partitioned variant

--- scripts/scaling.sh
+++ scripts/scaling.sh
@@ -8,16 +8,20 @@ for threads in $(seq 1 1 16); do
   params="--numQueries 0 --leafMethod rotations --leafSize 5 --bucketSize 5 --numThreads $threads"
   ./gpurecsplit_construction $params --numObjects $((2*threads))M
   ./simdrecsplit_construction $params --numObjects $((50*threads))M
+  ./simdrecsplit_construction $params --numObjects $((50*threads))M --partitioned
 
   params="--numQueries 0 --leafMethod rotations --leafSize 8 --bucketSize 100 --numThreads $threads"
   ./gpurecsplit_construction $params --numObjects $((20*threads))M
   ./simdrecsplit_construction $params --numObjects $((30*threads))M
+  ./simdrecsplit_construction $params --numObjects $((30*threads))M --partitioned
 
   params="--numQueries 0 --leafMethod rotations --leafSize 12 --bucketSize 9 --numThreads $threads"
   ./gpurecsplit_construction $params --numObjects $((5*threads))M
   ./simdrecsplit_construction $params --numObjects $((10*threads))M
+  ./simdrecsplit_construction $params --numObjects $((10*threads))M --partitioned
 
   params="--numQueries 0 --leafMethod rotations --leafSize 16 --bucketSize 2000 --numThreads $threads"
   ./gpurecsplit_construction $params --numObjects $((500*threads))k
   ./simdrecsplit_construction $params --numObjects $((20*threads))k
+  ./simdrecsplit_construction $params --numObjects $((20*threads))k --partitioned
 done
