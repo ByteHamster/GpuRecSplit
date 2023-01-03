e073fe89799ce33cf28150f6c9b618b1b664e37b
ByteHamster <info@bytehamster.com>
2023-01-03 14:38:38 +0100

    Multiple GPU threads no longer makes a difference

--- scripts/showcaseOverall.sh
+++ scripts/showcaseOverall.sh
@@ -13,11 +13,11 @@ function repeat() {
 repeat  2 ./recsplit_construction     --numObjects 1M --numQueries 50M --leafMethod bruteforce --leafSize 16 --bucketSize 2000
 repeat  3 ./simdrecsplit_construction --numObjects 1M --numQueries 50M --leafMethod rotations  --leafSize 16 --bucketSize 2000 --numThreads 1
 repeat  5 ./simdrecsplit_construction --numObjects 1M --numQueries 50M --leafMethod rotations  --leafSize 16 --bucketSize 2000 --numThreads 16
-repeat 10 ./gpurecsplit_construction  --numObjects 1M --numQueries 50M --leafMethod rotations  --leafSize 16 --bucketSize 2000 --numThreads 4
+repeat 10 ./gpurecsplit_construction  --numObjects 1M --numQueries 50M --leafMethod rotations  --leafSize 16 --bucketSize 2000 --numThreads 1
 
 repeat  1 ./recsplit_construction     --numObjects 1M --numQueries 50M --leafMethod bruteforce --leafSize 18 --bucketSize 50
 repeat  3 ./simdrecsplit_construction --numObjects 1M --numQueries 50M --leafMethod rotations  --leafSize 18 --bucketSize 50 --numThreads 1
 repeat  5 ./simdrecsplit_construction --numObjects 1M --numQueries 50M --leafMethod rotations  --leafSize 18 --bucketSize 50 --numThreads 16
-repeat 10 ./gpurecsplit_construction  --numObjects 1M --numQueries 50M --leafMethod rotations  --leafSize 18 --bucketSize 50 --numThreads 4
+repeat 10 ./gpurecsplit_construction  --numObjects 1M --numQueries 50M --leafMethod rotations  --leafSize 18 --bucketSize 50 --numThreads 1
 
-repeat  1 ./gpurecsplit_construction  --numObjects 1M --numQueries 50M --leafMethod rotations  --leafSize 24 --bucketSize 2000 --numThreads 4
+repeat  1 ./gpurecsplit_construction  --numObjects 1M --numQueries 50M --leafMethod rotations  --leafSize 24 --bucketSize 2000 --numThreads 1
