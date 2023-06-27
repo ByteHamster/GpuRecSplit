5aa38fc62628580fbc6bd40c143b0428ba88f324
ByteHamster <info@bytehamster.com>
2023-06-27 13:54:11 +0200

    Run experiments with larger N

--- scripts/showcaseOverall.sh
+++ scripts/showcaseOverall.sh
@@ -10,14 +10,16 @@ function repeat() {
     done
 }
 
-repeat  2 ./recsplit_construction     --numObjects 1M --numQueries 50M --leafMethod bruteforce --leafSize 16 --bucketSize 2000
-repeat  3 ./simdrecsplit_construction --numObjects 1M --numQueries 50M --leafMethod rotations  --leafSize 16 --bucketSize 2000 --numThreads 1
-repeat  5 ./simdrecsplit_construction --numObjects 1M --numQueries 50M --leafMethod rotations  --leafSize 16 --bucketSize 2000 --numThreads 16
-repeat 10 ./gpurecsplit_construction  --numObjects 1M --numQueries 50M --leafMethod rotations  --leafSize 16 --bucketSize 2000 --numThreads 1
+repeat 2 ./recsplit_construction     --numObjects 5M --numQueries 50M --leafMethod bruteforce --leafSize 16 --bucketSize 2000 --numThreads 1
+repeat 2 ./recsplit_construction     --numObjects 5M --numQueries 50M --leafMethod bruteforce --leafSize 16 --bucketSize 2000 --numThreads 16
+repeat 2 ./simdrecsplit_construction --numObjects 5M --numQueries 50M --leafMethod rotations  --leafSize 16 --bucketSize 2000 --numThreads 1
+repeat 3 ./simdrecsplit_construction --numObjects 5M --numQueries 50M --leafMethod rotations  --leafSize 16 --bucketSize 2000 --numThreads 16
+repeat 5 ./gpurecsplit_construction  --numObjects 5M --numQueries 50M --leafMethod rotations  --leafSize 16 --bucketSize 2000 --numThreads 1
 
-repeat  1 ./recsplit_construction     --numObjects 1M --numQueries 50M --leafMethod bruteforce --leafSize 18 --bucketSize 50
-repeat  3 ./simdrecsplit_construction --numObjects 1M --numQueries 50M --leafMethod rotations  --leafSize 18 --bucketSize 50 --numThreads 1
-repeat  5 ./simdrecsplit_construction --numObjects 1M --numQueries 50M --leafMethod rotations  --leafSize 18 --bucketSize 50 --numThreads 16
-repeat 10 ./gpurecsplit_construction  --numObjects 1M --numQueries 50M --leafMethod rotations  --leafSize 18 --bucketSize 50 --numThreads 1
+repeat 1 ./recsplit_construction     --numObjects 5M --numQueries 50M --leafMethod bruteforce --leafSize 18 --bucketSize 50 --numThreads 1
+repeat 1 ./recsplit_construction     --numObjects 5M --numQueries 50M --leafMethod bruteforce --leafSize 18 --bucketSize 50 --numThreads 16
+repeat 2 ./simdrecsplit_construction --numObjects 5M --numQueries 50M --leafMethod rotations  --leafSize 18 --bucketSize 50 --numThreads 1
+repeat 3 ./simdrecsplit_construction --numObjects 5M --numQueries 50M --leafMethod rotations  --leafSize 18 --bucketSize 50 --numThreads 16
+repeat 5 ./gpurecsplit_construction  --numObjects 5M --numQueries 50M --leafMethod rotations  --leafSize 18 --bucketSize 50 --numThreads 1
 
-repeat  1 ./gpurecsplit_construction  --numObjects 1M --numQueries 50M --leafMethod rotations  --leafSize 24 --bucketSize 2000 --numThreads 1
+repeat 1 ./gpurecsplit_construction  --numObjects 5M --numQueries 50M --leafMethod rotations  --leafSize 24 --bucketSize 2000 --numThreads 1
