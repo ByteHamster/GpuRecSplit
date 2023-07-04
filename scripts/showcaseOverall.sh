d2f5067b3912d6c155bda4d9ae2332a32c722b8d
ByteHamster <info@bytehamster.com>
2023-07-04 12:55:04 +0200

    Also run some benchmarks without rotation fitting

--- scripts/showcaseOverall.sh
+++ scripts/showcaseOverall.sh
@@ -15,11 +15,14 @@ repeat 2 ./recsplit_construction     --numObjects 5M --numQueries 50M --leafMeth
 repeat 2 ./simdrecsplit_construction --numObjects 5M --numQueries 50M --leafMethod rotations  --leafSize 16 --bucketSize 2000 --numThreads 1
 repeat 3 ./simdrecsplit_construction --numObjects 5M --numQueries 50M --leafMethod rotations  --leafSize 16 --bucketSize 2000 --numThreads 16
 repeat 5 ./gpurecsplit_construction  --numObjects 5M --numQueries 50M --leafMethod rotations  --leafSize 16 --bucketSize 2000 --numThreads 1
+repeat 5 ./gpurecsplit_construction  --numObjects 5M --numQueries 50M --leafMethod bruteforce  --leafSize 16 --bucketSize 2000 --numThreads 1
 
 repeat 1 ./recsplit_construction     --numObjects 5M --numQueries 50M --leafMethod bruteforce --leafSize 18 --bucketSize 50 --numThreads 1
 repeat 1 ./recsplit_construction     --numObjects 5M --numQueries 50M --leafMethod bruteforce --leafSize 18 --bucketSize 50 --numThreads 16
 repeat 2 ./simdrecsplit_construction --numObjects 5M --numQueries 50M --leafMethod rotations  --leafSize 18 --bucketSize 50 --numThreads 1
-repeat 3 ./simdrecsplit_construction --numObjects 5M --numQueries 50M --leafMethod rotations  --leafSize 18 --bucketSize 50 --numThreads 16
+repeat 5 ./simdrecsplit_construction --numObjects 5M --numQueries 50M --leafMethod rotations  --leafSize 18 --bucketSize 50 --numThreads 128
 repeat 5 ./gpurecsplit_construction  --numObjects 5M --numQueries 50M --leafMethod rotations  --leafSize 18 --bucketSize 50 --numThreads 1
+repeat 5 ./gpurecsplit_construction  --numObjects 5M --numQueries 50M --leafMethod bruteforce  --leafSize 18 --bucketSize 50 --numThreads 1
 
 repeat 1 ./gpurecsplit_construction  --numObjects 5M --numQueries 50M --leafMethod rotations  --leafSize 24 --bucketSize 2000 --numThreads 1
+repeat 1 ./gpurecsplit_construction  --numObjects 5M --numQueries 50M --leafMethod bruteforce  --leafSize 24 --bucketSize 2000 --numThreads 1
